984f0ed0349c505614ef4dfc26d040a20f9418e2
Atalay Erdoğan <110429589+atalayerdgn@users.noreply.github.com>
2023-07-12 08:44:35 +0300

    Add files via upload

---
+++ ft_lstmap.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerdogan <aerdogan@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 03:05:46 by aerdogan          #+#    #+#             */
+/*   Updated: 2023/07/12 03:05:47 by aerdogan         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*str;
+	t_list	*tmp;
+	void	*content;
+
+	if (!lst || !f)
+		return (NULL);
+	str = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		if (!content)
+			ft_lstclear(&str, del);
+		if (!content)
+			return (NULL);
+		tmp = ft_lstnew(content);
+		if (!tmp)
+		{
+			free(content);
+			ft_lstclear(&str, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&str, tmp);
+		lst = lst->next;
+	}
+	return (str);
+}
